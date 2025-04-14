afeba1b5d111c8e694d9c453c3be37a0233f075c
fadyfadd <fady_faddoul@hotmail.com>
2025-04-14 10:25:22 +0300

    initial commit

---
+++ src/main/java/com/training/training_event_management_system_back/TrainingEventManagementSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.training.training_event_management_system_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingEventManagementSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingEventManagementSystemBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/training_event_management_system_back/TrainingEventManagementSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.training_event_management_system_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingEventManagementSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
